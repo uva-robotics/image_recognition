2780bcb14c22ed38829347187ce9265002aacc04
Lut99 <mullertim99@gmail.com>
2018-06-09 14:48:38 +0200

    Updated to working face recognition :D

--- object_recognition/src/main.py
+++ object_recognition/src/main.py
@@ -10,19 +10,74 @@ import argparse
 import numpy as np
 import cv2
 import time
+import threading
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+# Checks for timeouts
+class TimeoutTracker (threading.Thread):
+    def __init__ (self, timeout=3):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.running = False
+        self.timeout = timeout
+        name = "TimeoutTracker"
+        id = 0
+        # Add the number
+        if name + str(id) in threading.enumerate():
+            id += 1
+        # Register
+        self.name = name + str(id)
+
+        self.log("Created")
+
+    # Print a message with the name as init
+    def log (self, message):
+        print("{} > {}".format(self.name, message))
+
+    def poke (self):
+        self.last_poke = time.time()
+
+    def start (self):
+        self.log("Starting...")
+        if not self.running:
+            self.last_poke = time.time()
+            self.running = True
+            threading.Thread.start(self)
+            self.log("Successfully started")
+        else:
+            self.log("Already started")
+
+    def run (self):
+        while self.running:
+            if time.time() - self.last_poke > self.timeout:
+                print("WARNING: No message received from Pepper in three seconds")
+
+    def stop (self):
+        self.log("Stopping...")
+        if self.running:
+            self.running = False
+
+            # Wait until done
+            while self.isAlive():
+                pass
+
+            self.log("Successfully stopped")
+        else:
+            self.log("Already stopped")
+
+# Class for recognising objects (although it does faces, for now)
 class Recogniser ():
     # INit
     def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+        self.face_cascade = cv2.CascadeClassifier('/projects/uva-robotics/src/image_recognition/object_recognition/src/haarcascade_frontalface_default.xml')
+        self.eye_cascade = cv2.CascadeClassifier('/projects/uva-robotics/src/image_recognition/object_recognition/src/haarcascade_eye.xml')
 
+    # Use both face_cascade and eye_cascade to recognise face and then eyes
     def classify (self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Test", gray)
+        cv2.imshow("Test", img)
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -31,9 +86,9 @@ class Recogniser ():
             eyes = self.eye_cascade.detectMultiScale(roi_gray)
             for (ex,ey,ew,eh) in eyes:
                 cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        cv2.waitKey(1)
         return img
 
+# Class for streaming video from pepper
 class VideoStreamer ():
     # INit
     def __init__(self, running_time = -1, filetype = "mp4"):
@@ -44,7 +99,8 @@ class VideoStreamer ():
         self.running_time = running_time
         self.extension = filetype
         self.recogniser = Recogniser()
-        print("VideoStreamer > Created")
+        self.timeout_tracker = TimeoutTracker()
+        print("VideoStreamer > Created (mode: Pepper webcam)")
 
     # Start
     def start (self):
@@ -58,6 +114,9 @@ class VideoStreamer ():
 
             # Setup video reader
             self.fourcc = cv2.VideoWriter_fourcc(*'X264')
+
+            # Start timeout timer
+            self.timeout_tracker.start()
 
             print("VideoStreamer > Started successfully")
 
@@ -82,11 +141,15 @@ class VideoStreamer ():
         if self.running:
             self.idle = False
 
+            # Set time the message was received
+            self.timeout_tracker.poke()
+
             if self.first_time:
                 self.first_time = False
                 # Finish initing video codec
-                self.out = cv2.VideoWriter("/home/tim/Desktop/output.mp4", self.fourcc, 24.0, (data.width, data.height))
-                print("VideoStreamer > Successfully inited videowriter")
+                self.out_raw = cv2.VideoWriter("/home/tim/Desktop/output.mp4", self.fourcc, 20.0, (data.width, data.height))
+                self.out_recognise = cv2.VideoWriter("/home/tim/Desktop/output_faces.mp4", self.fourcc, 20.0, (data.width, data.height))
+                print("VideoStreamer > Successfully inited videowriters")
 
             # Write away
             try:
@@ -95,13 +158,14 @@ class VideoStreamer ():
                 print(e)
 
             # Analyse the frame
-            self.out.write(cv_image)
-            recogniser_img = self.recogniser.classify(cv_image)
-            cv2.imshow("Img", recogniser_img)
-            cv2.waitKey(1)
-            #self.recogniser.classify(cv_image)
+            self.out_raw.write(cv_image)
+            face_img = self.recogniser.classify(cv_image)
+            self.out_recognise.write(face_img)
+            cv2.imshow("Img", face_img)
 
             self.idle = True
+
+            cv2.waitKey(1)
 
     # Stop it
     def stop (self):
@@ -117,24 +181,57 @@ class VideoStreamer ():
         else:
             print("VideoStreamer > Already closed")
 
+# Reads video from a file and shows it frame-by-frame
+class VideoStreamerFile ():
+    # INit
+    def __init__(self, path):
+        self.path = path
+
+        print("VideoStreamer > Created (mode: file)")
+
+    # Start streaming
+    def start (self):
+        cap = cv2.VideoCapture(self.path)
+
+        print("VideoStreamer > Opened cap (Press 'Q' to stop)")
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                cv2.imshow("Img", frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        print("VideoStreamer > Completed successfully")
+
 # Entry point
 if __name__ == '__main__':
     # Get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--runtime", type=int, help="The time (in seconds) the script will run")
     parser.add_argument("-f", "--filetype", help="The extension of the stream file (without .)")
+    parser.add_argument("-i", "--inputfile", help="The file that is to be used. Leave empty to use Pepper's camera")
     args = parser.parse_args()
 
     runtime = -1
     filetype = "mp4"
+    mode = "***PEPPER-CAMERA***"
     if args.runtime:
         runtime = args.runtime
     if args.filetype:
         filetype = args.filetype
+    if args.inputfile:
+        mode = args.inputfile
 
     # Init ROS
     rospy.init_node('object_recognition')
 
     # Start the streamer
-    streamer = VideoStreamer(runtime, filetype)
+    if mode == "***PEPPER-CAMERA***":
+        streamer = VideoStreamer(runtime, filetype)
+    else:
+        streamer = VideoStreamerFile(mode)
     streamer.start()
