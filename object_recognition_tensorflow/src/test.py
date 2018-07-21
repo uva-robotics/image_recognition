b31f6deb31f4149e223813a500222e6e0bc059d2
Tim Müller <mullertim99@gmail.com>
2018-07-21 18:34:04 +0200

    Did some tests related to lightnet

--- object_recognition_tensorflow/src/test.py
+++ object_recognition_tensorflow/src/test.py
@@ -5,26 +5,13 @@
 #
 # A nice script to test in
 
+import lightnet
 
-import cv2
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-print("{}HEADER{}".format(bcolors.HEADER, bcolors.ENDC))
-print("{}OKBLUE{}".format(bcolors.OKBLUE, bcolors.ENDC))
-print("{}OKGREEN{}".format(bcolors.OKGREEN, bcolors.ENDC))
-print("{}WARNING{}".format(bcolors.WARNING, bcolors.ENDC))
-print("{}FAIL{}".format(bcolors.FAIL, bcolors.ENDC))
-print("{}BOLD{}".format(bcolors.BOLD, bcolors.ENDC))
-print("{}UNDERLINE{}".format(bcolors.UNDERLINE, bcolors.ENDC))
-
-print(cv2.__version__)
+print("Loading weights...")
+model = lightnet.load("yolo")
+print("Loading image...")
+image = lightnet.Image.from_bytes(open('/VirtualShare/dog.jpg', 'rb').read())
+print("Classifying...")
+boxes = model(image)
+print(boxes)
+print("Done.")
